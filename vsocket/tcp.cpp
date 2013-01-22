80edc290fd56f6b6ff311c919957b31719d7b99c
Martin Schröder <info@nordiskamarknadssystem.se>
2013-01-22 06:30:56 +0100

    ..

--- vsocket/tcp.cpp
+++ vsocket/tcp.cpp
@@ -163,13 +163,17 @@ void TCPNode::run(){
 	Node::run();
 	
 	if(this->state & CON_STATE_CONNECTING){
-		if (::connect(socket, (struct sockaddr*)&_socket_addr, sizeof(_socket_addr)) == -1
+		if ((rc = ::connect(socket, (struct sockaddr*)&_socket_addr, sizeof(_socket_addr))) == -1
 			&& errno != EINPROGRESS) {
+				LOG("TCP: connection failed to "<<host<<":"<<port);
 				::close(socket);
 				state = CON_STATE_DISCONNECTED;
-		} else {
+		} else if(rc == 0){
 			LOG("TCP: successfully connected to "<<host<<":"<<port);
 			state = CON_STATE_ESTABLISHED; 
+		} else {
+			// in progress
+			return;
 		}
 	}
 	/*
