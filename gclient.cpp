6fcb730893857974c476b58ac12110735d616620
Martin Schröder <info@nordiskamarknadssystem.se>
2013-02-03 13:32:28 +0100

    ..

--- gclient.cpp
+++ gclient.cpp
@@ -123,7 +123,7 @@ int main(int argc, char* argv[])
 	//VSL::set_option("server_crt", "server.crt");
 	//VSL::set_option("server_key", "server.key");
 	//VSL::set_option("anonymity_level", "1");
-
+/*
 VSL::SOCKINFO info;
 VSL::VSOCKET socket = VSL::socket(); 
 list<URL> path; 
@@ -162,7 +162,7 @@ cout<<"Response: "<<response<<endl;
 cout<<"Response: "<<response<<endl;
 VSL::shutdown();
 return 0;
-
+*/
 	// find all peers
 	if(options[CONNECT].count() > 0){
 		vector<string> peers;
